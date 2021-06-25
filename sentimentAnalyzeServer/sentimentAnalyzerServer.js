093560ed9fed232d2f49574c294187a717ddd71c
regis garnier <regis.garnier@wanadoo.fr>
2021-06-25 16:21:39 +0000

    tmp

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv')
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;//maj ?
+    let api_url = process.env.API_URL;//maj ?
+
+    const NaturalLanguageUnderstandingV1=require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } =require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1({
+        version : '2020-08-01',
+        authentification : new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
 
 app.use(express.static('client'))
 
