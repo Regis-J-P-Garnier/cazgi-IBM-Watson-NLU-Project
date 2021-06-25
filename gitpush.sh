2366dac92354ec435e89c84929a33bfb39087b28
regis garnier <regis.garnier@wanadoo.fr>
2021-06-25 16:01:19 +0000

    tmp

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rep=$(pwd)
+cd "/home/project/cazgi-IBM-Watson-NLU-Project"
 git config --global user.email "regis.garnier@wanadoo.fr"&&
 git config --global user.name "regis garnier"&&
-git add . && git commit -m "tmp" && git push
+git add . && git commit -m "tmp"&& git push origin --all
+cd "${rep}"
